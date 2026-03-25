a3327e20f738c91f9993ce04dce89dab301ce282
federicoacker <acker.federico@gmail.com>
2026-03-25 14:08:23 +0100

    Esercizio bonus 5 completato

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -35,8 +35,21 @@ if(indexOfEd !== -1){ // Controllo che Ed sia presente, se il risultato è -1 de
 }
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-// Potremmo usare includes e risolverlo così o usare indexOf come abbiamo fatto prima;
-const isFabioPresent = (teachers.indexOf("Fabio") !== -1);
+// Potremmo usare includes e risolverlo così o usare indexOf come abbiamo fatto prima, potremmo anche farlo con un for volendo;
+const isFabioPresent = (teachers.indexOf("Fabio") !== -1); // deciso di usare indexOf
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+// oppure con for
+let teachersString2 = ''; //Creiamo la stringa vuota
+for(let i = 0; i < teachers.length; i++){ //Ciclo for che si fa il giro di tutto l'array
+  const currentTeacher = teachers[i];
+  if(i<teachers.length - 1){ // controlliamo di non trovarci all'ultimo indice dell'array, perché quando siamo lì, 
+  // vogliamo aggiungere alla nostra stringa, solo il nome contenuto nell'array, senza virgola + spazio dopo
+    teachersString2 += `${currentTeacher}, ` // Aggiungiamo il valore nell'indice i-esimo dell'array, seguito da una virgola e uno spazio
+  } else { // Qui siamo all'ultimo indice dell'array
+    teachersString2 += currentTeacher; // Aggiungiamo solo il valore all'ultimo indice dell'array invece di mettere virgola e spazio.
+  }
+}
+// oppure con join
+const teachersString3 = teachers.join(', ');
