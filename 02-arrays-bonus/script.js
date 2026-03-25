4600d5c6f3ed07a8f943adc68b19d6c203d099a9
federicoacker <acker.federico@gmail.com>
2026-03-25 13:54:54 +0100

    Esercizio bonus 2 completato

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -20,7 +20,12 @@ for (let i = teachers.length - 1; i >= 0; i--) { // Scorro al contrario l'array,
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+for(let i = 0; i < teachers.length; i++){
+  if(teachers[i].length >= 5){
+    longNames.push(teachers[i]);
+  }
+}
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
